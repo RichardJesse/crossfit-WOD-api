2f769ed86fcd6a3714d1e0517c36063fa03235a2
gradhav001 <jesse.richard@strathmore.edu>
2023-09-26 10:11:27 +0300

    configured the route set up the JSON in the database yet to cmplete the work

---
+++ src/services/workoutServices.js
@@ -0,0 +1,30 @@
+const workout = require("../database/Workout");
+
+
+const getAllWorkouts = () =>{
+    const allWorkouts = workout.getAllWorkouts;
+    return allWorkouts;
+}
+
+const getOneWorkout = () =>{
+    return;
+}
+const createNewWorkout = () =>{
+    return;
+}
+
+const updateOneWorkout = () =>{
+    return;
+}
+
+const deleteOneWorkout = () =>{
+    return;
+}
+
+module.exports = {
+    getAllWorkouts,
+    getOneWorkout,
+    createNewWorkout,
+    updateOneWorkout,
+    deleteOneWorkout
+};
